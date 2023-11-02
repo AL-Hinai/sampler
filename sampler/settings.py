f05f0c5ad3ab3c32f6555187741bd832adc4a8ea
AL-Hinai <msalim245@gmail.com>
2023-11-02 08:50:27 +0000

    deploy to PythonAnyWhere

--- sampler/settings.py
+++ sampler/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^or^gdq@phq7p4_98+x1)@c+vr2_9z)qeab*iz_0u%!1grgzkw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['msalim2.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
